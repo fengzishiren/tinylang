47d92ec3d6d6d821c2724ae9cb86f6f26b337fe3
fengzishiren <xiaoyaozi106@sina.com>
2014-09-17 15:34:32 +0800

    add interp

--- src/While.java
+++ src/While.java
@@ -1,13 +1,31 @@
-
-
 public class While extends Stmt {
 
 	public Node test;
 	public Node stmt;
+
 	public While() {
 	}
+
 	public void init(Node test, Node stmt) {
 		this.test = test;
 		this.stmt = stmt;
+	}
+
+	@Override
+	public Value interp(Scope s) {
+		return test.interp(s);
+	}
+
+	@Override
+	public Value typecheck(Scope s) {
+		Value v = test.typecheck(s);
+		if (!(v instanceof BoolType))
+			S.error("必须是boolean " + test);
+		return v;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("while (%s) %s", test, stmt);
 	}
 }
