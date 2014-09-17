64ebf019ffa97c2cf61d3e0396687da23d8615af
fengzishiren <xiaoyaozi106@sina.com>
2014-09-17 15:37:06 +0800

    print return value or exception

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 
-
 import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			Interpreter interpreter = new Interpreter("example.sl");
-			interpreter.interp();
+			Interpreter interpreter = new Interpreter("langs/test.tl");
+			String ret = interpreter.interp();
+			System.out.println(ret);
 		} catch (IOException e) {
-			System.out.println(e);
+			System.err.println(e);
 		}
 	}
 }
