03bcad423bf6b39ea4702dc8eb27991719bb3073
fengzishiren <xiaoyaozi106@sina.com>
2014-09-17 18:37:29 +0800

    puts Value

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-
 import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			Interpreter interpreter = new Interpreter("langs/test.tl");
-			String ret = interpreter.interp();
+			Interpreter interpreter = new Interpreter("langs/example.tl");
+			Value ret = interpreter.interp();
 			System.out.println(ret);
 		} catch (IOException e) {
 			System.err.println(e);
