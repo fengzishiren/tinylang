6106fa8923c9539c2ae2f0a4fbf9864e4ff049a6
fengzishiren <xiaoyaozi106@sina.com>
2014-09-17 16:29:43 +0800

    function invoke unit, function set

---
+++ src/Unit.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Unit extends Node {
+
+	private List<Fun> funcs = new ArrayList<>();
+
+	public void addFun(Fun fun) {
+		funcs.add(fun);
+	}
+
+	@Override
+	public Value interp(Scope s) {
+		int idx = 0;
+		Name main = null;
+		//每个closure共享初始Scope
+		for (Fun fun : funcs) {
+			Binder.define(fun.name, new Closure(fun, s), s);
+			if ("main".equals(fun.name.id)) {
+				idx++;
+				main = fun.name;
+			}
+		}
+		if (idx == 0) {
+			S.error("找不到main函数");
+		} else if (idx > 1) {
+			S.error("main函数重复定义在多处");
+		}
+		Call call = new Call(main, Argument.noArgs());
+		return call.interp(s);
+	}
+
+	@Override
+	public Value typecheck(Scope s) {
+		return Value.ANY;
+	}
+
+	@Override
+	public String toString() {
+		return U.join("\n", funcs);
+	}
+
+}
