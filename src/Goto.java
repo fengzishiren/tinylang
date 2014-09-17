eed93d0c4e48afb0828679db1bc8c633cbb12018
fengzishiren <xiaoyaozi106@sina.com>
2014-09-17 20:33:56 +0800

    jmp loop or Capture the return value by throwing exception

---
+++ src/Goto.java
@@ -0,0 +1,11 @@
+/**
+ * 跳出最近的loop
+ * @author lunatic
+ *
+ */
+public class Goto extends RuntimeException {
+
+	public Goto() {
+		super();
+	}
+}
---
+++ src/ReturnJmp.java
@@ -0,0 +1,25 @@
+/**
+ * 跳出层层block 直到最近的函数
+ * 
+ * @author lunatic
+ *
+ */
+public class ReturnJmp extends Goto {
+	public Value value = Value.VOID;
+
+	/**
+	 * return ; 则返回Void
+	 * @return
+	 */
+	public Value attatchment() {
+		return value;
+	}
+
+	public ReturnJmp(Value value) {
+		super();
+		this.value = value;
+	}
+
+	public ReturnJmp() {
+	}
+}
