b75342902d18c0a54897bed7dae4d8db53887b4e
fengzishiren <xiaoyaozi106@sina.com>
2014-09-17 16:43:59 +0800

    add descOf: get desc of tag

--- src/Tag.java
+++ src/Tag.java
@@ -1,25 +1,50 @@
 
-
 public class Tag {
-	public static final int BASIC = 1;
-	public static final int AND = 2;
-	public static final int OR = 3;
-	public static final int INT = 4;
-	public static final int FLOAT = 0;
-	public static final int ID = 5;
-	public static final int STRING = 6;
-	public static final int IF = 7;
-	public static final int ELSE = 8;
-	public static final int GE = 9;
-	public static final int LE = 10;
-	public static final int NE = 11;
-	public static final int EQ = 12;
-	public static final int WHILE = 11;
-	public static final int BREAK = 12;
-	public static final int DO = 13;
-	public static final int FALSE = 14;
-	public static final int TRUE = 15;
-	public static final int DEFINE = 20;
-	public static final int RETURN = 21;
+	public static final int INT = 256;
+	public static final int STRING = 257;
+	public static final int FLOAT = 258;
+	public static final int AND = 259;
+	public static final int OR = 260;
+	public static final int ID = 261;
+	public static final int IF = 262;
+	public static final int ELSE = 263;
+	public static final int GE = 264;
+	public static final int LE = 265;
+	public static final int NE = 266;
+	public static final int EQ = 267;
+	public static final int WHILE = 268;
+	public static final int BREAK = 269;
+	public static final int DO = 270;
+	public static final int FALSE = 271;
+	public static final int TRUE = 272;
+	public static final int DEFINE = 273;
+	public static final int RETURN = 274;
+
+	public static final String[] desc = {
+		"int",
+		"string",
+		"float",
+		"and",
+		"or",
+		"id",
+		"if",
+		"else",
+		"ge",
+		"le",
+		"ne",
+		"eq",
+		"while",
+		"break",
+		"do",
+		"false",
+		"true",
+		"define",
+		"return",
+		"desc" 
+	};
+
+	public static String descOf(int tag) {
+		return tag > 255 ? desc[tag - 256]: String.valueOf((char)tag);
+	}
 
 }
