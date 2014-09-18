32ddc6d5068d72be696cafa7f5ddc2b2dc35f20a
fengzishiren <xiaoyaozi106@sina.com>
2014-09-18 23:18:37 +0800

    add type built-in func

---
+++ src/TypeFun.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class TypeFun extends BuiltinFun {
+
+	public TypeFun() {
+		super("type", 1);
+	}
+
+	@Override
+	public Value apply(List<Value> args) {
+		return args.get(0).type();
+	}
+
+	@Override
+	public Type type() {
+		return Type.builtinFuntion;
+	}
+
+
+}
