b9f1aaba8da687197686777d1a3e6d7252ff0778
fengzishiren <xiaoyaozi106@sina.com>
2014-09-18 14:55:59 +0800

    builtin Zero

--- src/Node.java
+++ src/Node.java
@@ -1,13 +1,17 @@
-
-
 public abstract class Node {
 
+	/**
+	 * 
+	 *  -3, -8, +3 => 0 - 3, 0 - 8, 0 + 3
+	 */
+	public static final Node zero = new Zero();
+
 	public abstract Value interp(Scope s);
-	
+
 	public static Value interp(Node node, Scope s) {
 		return node.interp(s);
 	}
-	
+
 	public abstract Value typecheck(Scope s);
 
 }
