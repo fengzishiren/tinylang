4b1a3d7078d65524ba5c5e3281dacf197d9a9df2
fengzishiren <xiaoyaozi106@sina.com>
2014-09-21 15:29:04 +0800

    improve error

--- src/S.java
+++ src/S.java
@@ -1,10 +1,14 @@
 public class S {
 
 	public static void error(String msg) {
+		System.err.println(msg);
+		//Thread.dumpStack();
+		//new SyntaxException(msg).printStackTrace();
+		//System.exit(1);
 		throw new SyntaxException(msg);
 	}
 
-	public static void error(String msg, Object... args) {
-		throw new SyntaxException(String.format(msg, args));
-	}
+ 	public static void error(String msg, Object... args) {
+ 		error(String.format(msg, args));
+ 	}
 }
