20bfc518058f3516ac3fd78b53c8fa83c34fd832
fengzishiren <xiaoyaozi106@sina.com>
2014-10-01 20:55:22 +0800

    add main driver

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -10,10 +10,19 @@ public class Interpreter {
 	public Value interp() throws IOException {
 		Parser parser = new Parser(new Lexer(U.readFile(file)));
 		Node root = parser.parse();
-		//print ast
+		// print ast
 		System.out.println(root.toString());
 		Value retval = root.interp(Scope.initScope());
 		return retval;
 	}
 
+	public static void main(String[] args) {
+		try {
+			Interpreter interpreter = new Interpreter(args[0]);
+			interpreter.interp();
+		} catch (Exception e) {
+			System.err.println(e);
+			System.exit(1);
+		}
+	}
 }
